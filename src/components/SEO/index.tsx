724de084212b167c501c08bd1f68046907013cbf
sozinho <soaresjonatas398@gmail.com>
2022-08-20 17:01:35 +0000

    ASDASDASDASD

--- src/components/SEO/index.tsx
+++ src/components/SEO/index.tsx
@@ -2,17 +2,22 @@ import React from 'react'
 import Head from 'next/head'
 
 interface Props {
-  title: string
-  description: string
+	title: string
+	description: string
 }
 
 export const SEO = ({ title, description }: Props) => {
-  return (
-    <Head>
-      {/* Primary Meta Tags */}
-      <title>{title}</title>
-      <meta name="title" content={title} />
-      <meta name="description" content={description} />
-    </Head>
-  )
+	return (
+		<Head>
+			{/* Primary Meta Tags */}
+			<title>{title}</title>
+			<meta name="title" content={title} />
+			<meta name="description" content={description} />
+			<script
+				async
+				src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4515639184646084"
+				crossOrigin="anonymous"
+			/>
+		</Head>
+	)
 }
