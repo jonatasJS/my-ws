5b33d592a193500147663c561f15ed2dcdc7b2c1
sozinho <soaresjonatas398@gmail.com>
2022-08-24 15:30:25 +0000

    create pages [password_reset,register]

---
+++ src/pages/password_reset/index.page.tsx
@@ -0,0 +1,9 @@
+export default function PassReset() {
+  return (
+    <>
+      <div>
+        <h1>password_reset</h1>
+      </div>
+    </>
+  );
+}
---
+++ src/pages/register/index.page.tsx
@@ -0,0 +1,9 @@
+export default function Register() {
+  return (
+		<>
+			<div>
+				<h1>register</h1>
+			</div>
+		</>
+	)
+}
