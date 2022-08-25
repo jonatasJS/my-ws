2a8ca4ab08d027945e95ebbc2d0fc5e3bc8488cf
jonatasjs <soaresjonatas398@gmail.com>
2022-08-25 10:39:20 -0300

    add adsense and analytics

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import type { DocumentContext } from 'next/document'
 import NextDocument, { Head, Html, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
+import Script from 'next/script'
 
 export default class MyDocument extends NextDocument {
 	static async getInitialProps(ctx: DocumentContext) {
@@ -34,12 +35,39 @@ export default class MyDocument extends NextDocument {
 		return (
 			<Html lang="pt-BR">
 				<Head>
-					<link rel="preconnect" href="https://fonts.googleapis.com"/>
-					<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='' />
-					<link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;500;600;700&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet"/>
+					<link rel="preconnect" href="https://fonts.googleapis.com" />
+					<link
+						rel="preconnect"
+						href="https://fonts.gstatic.com"
+						crossOrigin=""
+					/>
+					<link
+						href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;500;600;700&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
+						rel="stylesheet"
+					/>
 					<link rel="manifest" href="manifest.json" />
 					<link rel="icon" href="/favicon.svg" />
 					<meta name="theme-color" content="#080a0c" />
+					<Script
+						async
+						src="https://www.googletagmanager.com/gtag/js?id=G-1EP4FEX5C4"
+					/>
+					<Script
+						async
+						src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4515639184646084"
+						crossOrigin="anonymous"
+					/>
+					<Script
+						async
+						dangerouslySetInnerHTML={{
+							__html: `
+						window.dataLayer = window.dataLayer || [];
+						function gtag(){dataLayer.push(arguments);}
+						gtag('js', new Date());
+
+							gtag('config', 'G-1EP4FEX5C4');`,
+						}}
+					/>
 				</Head>
 				<body>
 					<Main />
